aaf0ba71b2766c536689b6df5cd8a815ad1efc7f
Louis Johnson <louis.johnson@comparethemarket.com.au>
2019-02-10 21:49:50 +1000

    refactoring

--- AStar.cs
+++ AStar.cs
@@ -1,111 +1,107 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Pathfinding
 {
   class AStar
   {
-    public Node[] FindPath(Node startNode, Node targetNode, Node[,] grid)
-    {
+    public int Heuristic(Node a, Node b) {
+      int x = Mathf.Abs(a.mapPosition.x - b.mapPosition.x);
+      int y = Mathf.Abs(a.mapPosition.y - b.mapPosition.y);
+      return x + y;
+    }
+
+    public Node[] GetPath(Node endNode) {
+        List<Node> path = new List<Node>();
+        Node temp = endNode;
+        path.Add(temp);
+        while (temp.previous != null) {
+            path.Add(temp.previous);
+            temp = temp.previous;
+        }
+        return path.ToArray();
+    }
+
+    public Node FindPath(Node[,] map, Node start, Node target) {
       List<Node> openNodes = new List<Node>();
       List<Node> closedNodes = new List<Node>();
-      openNodes.Add(startNode);
-      while (openNodes.Count > 0)
-      {
+      start.previous = null;
+      openNodes.Add(start);
+      while (openNodes.Count > 0) {
         Node currentNode = openNodes[0];
-        for (int i = 0; i < openNodes.Count; i++)
-        {
-          if (openNodes[i].f < currentNode.f)
-          {
+        for (int i = 0; i < openNodes.Count; i++) {
+          if (openNodes[i].f < currentNode.f) {
             currentNode = openNodes[i];
           }
-        }
-        if (currentNode == targetNode)
-        {
-          // found path
-          return GetPath(currentNode).ToArray();
-        }
-        closedNodes.Add(currentNode);
-        openNodes.Remove(currentNode);
 
-        foreach (Node neighbour in currentNode.GetNeighbours(grid))
-        {
-          if (!closedNodes.Contains(neighbour) && neighbour.walkable)
-          {
-            int tempG = currentNode.g + 1;
-            if (openNodes.Contains(neighbour))
-            {
-              if (tempG < neighbour.g)
-              {
-                neighbour.g = tempG;
-                neighbour.previous = currentNode;
-              }
+          if (openNodes[i].f == currentNode.f) {
+            if (openNodes[i].g > currentNode.g) {
+                currentNode = openNodes[i];
             }
-            else
-            {
-              neighbour.g = tempG;
-              neighbour.previous = currentNode;
-              openNodes.Add(neighbour);
-            }
-            neighbour.h = CalcHeuristic(neighbour, targetNode);
-            neighbour.f = neighbour.g + neighbour.h;
-
           }
-
         }
+
+        if (currentNode == target) {
+          return currentNode;
+        }
+
+        openNodes.Remove(currentNode);
+        closedNodes.Add(currentNode);
+        foreach(Node neighbour in currentNode.GetNeighbours(map)) {
+          if (!closedNodes.Contains(neighbour)) {
+            int tempG = currentNode.g + Heuristic(neighbour, currentNode);
+            if (!openNodes.Contains(neighbour)) {
+                openNodes.Add(neighbour);
+            } else if (tempG >= neighbour.g) {
+              continue;
+            }
+
+            neighbour.g = tempG;
+            neighbour.h = Heuristic(neighbour, target);
+            neighbour.f = neighbour.g + neighbour.h;
+            neighbour.previous = currentNode;
+          }
+        }
+
+
       }
-      // unable to find a path
       return null;
     }
 
-    List<Node> GetPath(Node current)
-    {
-      List<Node> path = new List<Node>();
-      Node temp = current;
-      while (temp.previous != null)
-      {
-        path.Add(temp);
-        temp = temp.previous;
-      }
-      return path;
-    }
-
-    int CalcHeuristic(Node startNode, Node targetNode)
-    {
-      int x = Mathf.Abs(startNode.gridPosition.x - targetNode.gridPosition.x);
-      int y = Mathf.Abs(startNode.gridPosition.y - targetNode.gridPosition.y);
-      return x + y;
-    }
   }
 
-  class Node
-  {
-    public Vector2Int gridPosition;
-    public int f = 0;
-    public int g = 0;
+  class Node {
     public int h = 0;
-    public Node previous = null;
+    public int g = 0;
+    public int f = 0;
+    public int vh = 0;
     public bool walkable = true;
-    public Node(Vector2Int _gridPosition)
-    {
-      gridPosition = _gridPosition;
+    public Node previous = null;
+    public Vector2Int mapPosition;
+    public Node(Vector2Int mapPosition) {
+      this.mapPosition = mapPosition;
     }
 
-    public List<Node> GetNeighbours(Node[,] grid)
-    {
-      List<Node> neighbours = new List<Node>();
-      Vector2Int[] vector2Ints = { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
-      foreach (Vector2Int vector2Int in vector2Ints)
-      {
-        Vector2Int possibleNeighbourPosition = vector2Int + gridPosition;
-        if (possibleNeighbourPosition.x >= 0 &&
-            possibleNeighbourPosition.y >= 0 &&
-            possibleNeighbourPosition.x <= grid.GetUpperBound(0) &&
-            possibleNeighbourPosition.y <= grid.GetUpperBound(1))
+    public Node[] GetNeighbours(Node[,] map) {
+        List<Node> neighbours = new List<Node>();
+        Vector2Int[] vector2Ints = { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left };
+        foreach (Vector2Int vector2Int in vector2Ints)
         {
-          neighbours.Add(grid[possibleNeighbourPosition.x, possibleNeighbourPosition.y]);
+            Vector2Int possibleNeighbourPosition = vector2Int + mapPosition;
+            if (possibleNeighbourPosition.x >= 0 &&
+                possibleNeighbourPosition.y >= 0 &&
+                possibleNeighbourPosition.x <= map.GetUpperBound(0) &&
+                possibleNeighbourPosition.y <= map.GetUpperBound(1))
+            {
+                Node neighbour = map[possibleNeighbourPosition.x, possibleNeighbourPosition.y];
+                if (neighbour.walkable) {
+                    neighbours.Add(neighbour);
+                }
+            }
         }
-      }
-      return neighbours;
+
+        return neighbours.ToArray();
     }
   }
 }
